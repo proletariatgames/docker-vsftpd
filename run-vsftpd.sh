22e3fd2dbccc1059afeded43b1df1a4e5585f9c8
Dan Brakeley <dbrakeley@proletariat.com>
2020-02-12 15:14:35 -0500

    allow anon user

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -18,8 +18,9 @@ else
 fi
 
 # Create home dir and update vsftpd user db:
-mkdir -p "/home/vsftpd/${FTP_USER}"
-chown -R ftp:ftp /home/vsftpd/
+mkdir -p /home/vsftpd/builds
+chown -R ftp:ftp /home/vsftpd/builds
+chmod a-w /home/vsftpd
 
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
