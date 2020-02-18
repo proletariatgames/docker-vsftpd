02e64883bd75f2879a9284ac5c0afee32118851e
Dan Brakeley <dbrakeley@proletariat.com>
2020-02-18 13:23:59 -0500

    don't blow away conf changes on restart

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
-
-# If no env var has been specified, generate a random password for FTP_USER:
-if [ "$FTP_PASS" = "**Random**" ]; then
-    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
-fi
-
 # Do not log to STDOUT by default:
 if [ "$LOG_STDOUT" = "**Boolean**" ]; then
     export LOG_STDOUT=''
@@ -19,25 +9,40 @@ fi
 
 # Create home dir and update vsftpd user db:
 mkdir -p /home/vsftpd/builds
-chown -R ftp:ftp /home/vsftpd/builds
-chmod a-w /home/vsftpd
+sudo chown -R ftp:ftp /home/vsftpd
+sudo chmod a-w /home/vsftpd
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
-/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+# only run this the first time, if we restart the server, assume it is already configured
+if [ ! -f "/etc/vsftpd/virtual_users.txt" ]; then
 
-# Set passive mode parameters:
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+	# If no env var for FTP_USER has been specified, use 'admin':
+	if [ "$FTP_USER" = "**String**" ]; then
+	    export FTP_USER='admin'
+	fi
+
+	# If no env var has been specified, generate a random password for FTP_USER:
+	if [ "$FTP_PASS" = "**Random**" ]; then
+	    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
+	fi
+
+	echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+	/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+
+	# Set passive mode parameters:
+	if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
+	    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
+	fi
+
+	echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
+	echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
+	echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
+	echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
+	echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
+
 fi
-
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
-echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
 
 # Get log file path
 export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
